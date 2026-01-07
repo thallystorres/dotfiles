ce077b09a33da6d5354d92e75525e3c0aa3d19ff
Thallys <thallys.auugusto.torres@gmail.com>
2026-01-07 11:52:14 -0300

    feature: adding zsh, nvim, ghostty, flatpak and tmux installation and configuration

--- install.sh
+++ install.sh
@@ -31,28 +31,35 @@ logwarn() {
   printf "🟡 ${YELLOW}%s${RESET}\n" "$1"
 }
 
+# ==========================================
 # 1. Package Manager Detection
+# ==========================================
 
 PACKAGER=""
 INSTALL_CMD=""
 UPDATE_CMD=""
+GHOSTTY_CMD=""
 
 if command -v dnf >/dev/null 2>&1; then
   loginfo "RPM based system detected (Fedora/RHEL)"
   PACKAGER="dnf"
   INSTALL_CMD="sudo dnf install -y"
   UPDATE_CMD="sudo dnf upgrade --refresh -y"
+  GHOSTTY_CMD="sudo bash -c 'loginfo Installing Ghostty with dnf && dnf install -y dnf-plugins-core && dnf copr enable -y pgdev/ghostty && dnf install -y ghostty'"
 elif command -v apt >/dev/null 2>&1; then
   loginfo "Debian based system detected (Ubuntu/Pop/Mint)."
   PACKAGER="apt"
   INSTALL_CMD="sudo apt install -y"
   UPDATE_CMD="sudo apt update && sudo apt upgrade -y"
+  GHOSTTY_CMD="sudo bash -c 'apt update && apt install -y curl ca-certificates gnupg && curl -fsSL https://repo.ghostty.org/apt/gpg.key | gpg --dearmor -o /usr/share/keyrings/ghostty.gpg && echo "deb [signed-by=/usr/share/keyrings/ghostty.gpg] https://repo.ghostty.org/apt stable main" > /etc/apt/sources.list.d/ghostty.list && apt update && apt install -y ghostty'"
 else
   logerror "Package manager not supported. Exiting script."
   exit 1
 fi
 
+# ==========================================
 # 2. System Update and Packages
+# ==========================================
 
 loginfo "Updating system..."
 eval $UPDATE_CMD
@@ -114,15 +121,112 @@ if [ -f "$PACKAGES_DIR/flatpaks.txt" ]; then
   flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
   loginfo "Installing Flatpaks..."
-  grep -vE '^\s*$' "$PACKAGES_DIR/flatpaks.txt" | while read -r app; do
-    flatpak install -y flathub "$app"
-  done
+  xargs -a "$PACKAGES_DIR/flatpaks.txt" flatpak install -y
 else
   logwarn "Flatpak list not found."
 fi
 
 # ==========================================
-# 5. Dotfiles Links (NO BACKUP)
+# 5. Ghostty config
+# ==========================================
+
+eval $GHOSTTY_CMD
+
+# ==========================================
+# 6. Zsh config
+# ==========================================
+
+# Set ZSH as default shell if installed
+if command -v zsh >/dev/null; then
+  if [ "$SHELL" != "$(which zsh)" ]; then
+    loginfo "Setting ZSH as default shell..."
+    chsh -s "$(which zsh)"
+  fi
+fi
+
+loginfo "Settings Zsh and Oh My Zsh"
+if [ ! -d "$HOME/.oh-my-zsh/" ]; then
+  loginfo "Installing Oh My Zsh..."
+  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+else
+  loginfo "Oh My Zsh already installed."
+ fi
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+loginfo "🔌 Adding Zsh plugins..."
+if [ ! -d "${ZSH_CUSTOM}/plugins/zsh-autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM}/plugins/zsh-autosuggestions"
+fi
+
+if [ ! -d "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting"
+fi
+
+# ==========================================
+# 7. Lazy.nvim config
+# ==========================================
+
+loginfo "🐘 Setting up NeoVim and LazyVim"
+LAZY_PATH="$HOME/.local/share/nvim/lazy/lazy.nvim"
+if [ ! -d "$LAZY_PATH" ]; then
+  loginfo "Adding Neovim plugin manager lazy.vim"
+  git clone https://github.com/folke/lazy.nvim.git --filter=blob:none "$LAZY_PATH"
+fi
+
+# ==========================================
+# 8. Tmux Plugin Manager config
+# ==========================================
+
+loginfo "🔄 Installing TPM for tmux..."
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+# ==========================================
+# 9. Language package managers
+# ==========================================
+
+if ! command -v pyenv &> /dev/null; then
+  # Pyenv e uv
+  loginfo "Installing Pyenv and uv..."
+  rm -Rf "${HOME}/.pyenv"
+  curl -fsSL https://pyenv.run | bash
+else
+  loginfo "Pyenv already installed..."
+fi
+
+if ! command -v uv &> /dev/null; then
+  # UV 
+  loginfo "Installing uv..."
+  curl -LsSf https://astral.sh/uv/install.sh | sh
+else
+  loginfo "UV already installed..."
+fi
+
+if ! command -v nvm &> /dev/null; then
+  # NVM
+  rm -Rf "${HOME}/.nvm"
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+else
+  loginfo "NVM already installed..."
+fi
+
+echo -e "
+[1;33mAttention: Manual steps ahead:[0m"
+echo ""
+echo "Open another terminal - DO NOT USE THIS INSTACE"
+echo ""
+echo "1. Execute 'nvm install --lts'"
+echo "2. Execute 'nvm install-latest-npm'"
+echo "3. Execute 'npm i -g prettier'"
+echo "4. Execute 'pyenv install 3.14' (or latest version)"
+echo "5. Execute 'pyenv global 3.14' (or latest version)"
+echo "6. Execute 'uv tool install pyright'"
+echo "7. Execute 'uv tool install ruff'"
+echo ""
+read -p "When you are done with the above tasks, press any key to continue..."
+
+# ==========================================
+# 10. Dotfiles Links (NO BACKUP)
 # ==========================================
 
 loginfo "Creating symlinks (Overwriting existing files)..."
@@ -186,15 +290,8 @@ fi
 link_file "$DOTFILES_DIR/dotfile/vim/.vimrc" "$HOME/.vimrc"
 
 # ==========================================
-# 6. Finalization
+# 11. Finalization
 # ==========================================
 
-# Set ZSH as default shell if installed
-if command -v zsh >/dev/null; then
-  if [ "$SHELL" != "$(which zsh)" ]; then
-    loginfo "Setting ZSH as default shell..."
-    chsh -s "$(which zsh)"
-  fi
-fi
 
 loginfo "Installation complete! Restart your terminal or log out."
